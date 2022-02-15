f4cacee50dbcb141f844f44f598bd08e427f5884
smnshuvo <shouman@exabyting.com>
2022-02-15 13:02:54 +0600

    Created model and added service layer

---
+++ home/src/main/java/expense/tracker/home/model/HomeMember.java
@@ -0,0 +1,16 @@
+package expense.tracker.home.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class HomeMember {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String contactNo;
+    private String avatarUrl;
+}
